f2efc7e5183cd6e7c8652b0e63a55de739fc59ec
unknown <7883121@naver.com>
2024-10-31 13:46:29 +0900

    App 라우터 수정

--- mapp2/src/App.js
+++ mapp2/src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
+//import { HashRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import React, { useState, useEffect, Navigate } from 'react';
 import logo from './logo.svg';
 import Home from './Home';
 import Main from './Main';
 import './App.css';
 
 function App() {
+    //API 키 디렉토리 접근
+    //const clientId = process.env.REACT_APP_NAVER_MAP_CLIENT_ID;
+
     // 네이버 지도 API 스크립트 로드
     useEffect(() => {
       const loadNaverMapScript = () => {
         const script = document.createElement('script');
-        script.src = 'https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=네이버지도키';
+        script.src = 'https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${data.clientId}';
         script.async = true;
         document.head.appendChild(script);
       };
@@ -21,8 +25,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/Home" element={<Home/>} />
-        <Route path="/Main" element={<Main/>} />
+        {/* <Route path="/" element={<Navigate to="/Home" />} />  */}
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Main" element={<Main />} />
       </Routes>
     </Router>
   );
